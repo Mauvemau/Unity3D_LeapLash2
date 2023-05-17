bcfef5f9f363f7426e1c1465c23b0708368e9a6b
Mau <mauvepinktree@gmail.com>
2023-05-17 06:01:05 -0300

    Removed unused function from scene manager

--- Assets/Scripts/Scenes/MySceneManager.cs
+++ Assets/Scripts/Scenes/MySceneManager.cs
@@ -9,26 +9,6 @@ public class MySceneManager : MonoBehaviourSingleton<MySceneManager>
     [Header("Event Listeners")]
     [SerializeField] private StringEventChannel loadSceneChannel;
 
-    private LightingSettings CreateDefaultLightingSettings()
-    {
-        LightingSettings lightingSettings = new LightingSettings();
-
-        // Baking settings
-        lightingSettings.bakedGI = true;
-        lightingSettings.realtimeGI = false;
-
-        // Indirect lighting settings
-        lightingSettings.indirectResolution = 2;
-        lightingSettings.lightmapResolution = 10;
-
-        // Ambient lighting settings
-        lightingSettings.aoExponentIndirect = 1.2f;
-        lightingSettings.aoExponentDirect = 1.5f;
-        lightingSettings.aoMaxDistance = 5f;
-
-        return lightingSettings;
-    }
-
     private void LoadScene(string sceneName)
     {
         if(SceneManager.GetActiveScene().name == "Game")
